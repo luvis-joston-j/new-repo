e93156da87bf2b4683aacf7c1c1bd00016c39344
luvis <luvisjoston@gmail.com>
2024-12-07 00:58:10 +0530

    mm

--- src/main/java/com/project/staragile/banking/BankappApplication.java
+++ src/main/java/com/project/staragile/banking/BankappApplication.java
@@ -1,13 +1,13 @@
-package com.project.staragile.banking;
+package com.example.bankapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankingApplication {
+public class BankappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankingApplication.class, args);
+		SpringApplication.run(BankappApplication.class, args);
 	}
 
 }
